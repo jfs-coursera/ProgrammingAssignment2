e33df268352aed23779ff965202b3cd86ef91f61
jfs <dummy.email@dummy.com>
2014-06-20 01:18:12 +0200

    Changes in set() function, and a test() function added.

--- cachematrix.R
+++ cachematrix.R
@@ -10,22 +10,27 @@
 
 ## Create a matrix, or retrieve it from cache if already defined
 ## Parameters:
-##   - x : the matrix object
+##   - mymatrix : the matrix object
 ## Returns:
 ##   - a list with environment information (???)
+## Example usage:
+##   m <- makeCacheMatrix(matrix(1:4, 2, 2))
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(mymatrix = matrix()) {
     ## By default, the inverse of the matrix is empty
     inv <- NULL
 
     ## Set the contents of the matrix
-    set <- function(y) {
-        x <<- y
-        inv <<- NULL
+    set <- function(newmatrix) {
+        ## Reset the matrix only if it has changed
+        if (!isTRUE(all.equal(mymatrix, newmatrix))) {
+            mymatrix <<- newmatrix
+            inv <<- NULL
+        }
     }
 
     ## Return the matrix object
-    get <- function() x
+    get <- function() mymatrix
 
     ## Set the inverse of the matrix
     setinverse <- function(solve) inv <<- solve
@@ -43,14 +48,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Compute the inverse of a matrix, or retrieve it from cache if already computed
 ## Parameters:
-##   - x : the matrix object
+##   - mymat : the matrix object
 ##   - ... : more arguments if needed
 ## Returns:
-##   - a matrix object, the inverse matrix of x
+##   - a matrix object, the inverse matrix of mymat
+## Example usage:
+##   m <- makeCacheMatrix(matrix(1:4, 2, 2))
+##   i <- cacheSolve(m)
 
-cacheSolve <- function(x, ...) {
-    ## Try and get the inverse of matrix x from cache
-    inv <- x$getinverse()
+cacheSolve <- function(mymat, ...) {
+    ## Try and get the inverse of matrix from cache
+    inv <- mymat$getinverse()
 
     ## Do we have the inverse matrix already?
     if (!is.null(inv)) {
@@ -62,26 +70,80 @@ cacheSolve <- function(x, ...) {
     ## Otherwise, inverse has to be computed. Let's go:
 
     ## Retrieve the matrix 
-    data <- x$get()
+    data <- mymat$get()
 
-    ## compute the inverse of the matrix (see R docs at
-    ## http://cran.r-project.org/doc/manuals/R-intro.html#Linear-equations-and-inversion)
+    ## Compute the inverse of the matrix
     inv <- solve(data, ...)
 
-    ## store the value in the cache
-    x$setinverse(inv)
+    ## Store the value in the cache
+    mymat$setinverse(inv)
 
-    ## return computed inverse
+    ## Return computed inverse
     inv
 }
 
 
 
+
 ## Testing
-## m <- makeCacheMatrix(matrix(1:4, 2, 2))
-## i <- makeCacheMatrix(cacheSolve(m))
-## cacheSolve(i)
-## => must return the same matrix as matrix(1:4, 2, 2)
+## This is not part of the assignment, but it helps...
+test <- function() {
+    ## some matrices (names in uppercase, so they are easier to spot)
+    MATRIX1 <- matrix(1:4, 2, 2)
+    MATRIX2 <- matrix(1:4, 2, 2)  ## the same as mat1
+    MATRIX3 <- matrix(c(1, 5, 2, 8), 2, 2)
 
+    print("-- create a matrix")
+    m <- makeCacheMatrix(MATRIX1)
 
+    print("-- get the matrix")
+    z <- m$get()
+    print(z)
+
+    print("-- get the inverse (shoud be NULL, because not yet computed)")
+    z <- m$getinverse()
+    print(z)
+
+    print("-- compute the inverse (we see the return value here)")
+    z <- cacheSolve(m)
+    print(z)
+
+    print("-- get the inverse (should not be NULL now)")
+    z <- m$getinverse()
+    print(z)
+
+    print("-- multiply matrix by its inverse (should obtain identity matrix)")
+    z <- m$get() %*% m$getinverse()
+    print(z)
+
+    print("-- set the matrix, with the same values as above")
+    m$set(MATRIX2)
+    z <- m$get()
+    print(z)
+
+    print("-- get the inverse (shoud not be NULL because actually, the matrix has not changed)")
+    z <- m$getinverse()
+    print(z)
+
+    print("-- set the matrix, with other values")
+    m$set(MATRIX3)
+    z <- m$get()
+    print(z)
+
+    print("-- get the inverse (shoud be NULL)")
+    z <- m$getinverse()
+    print(z)
+
+    print("-- compute the inverse (we see the return value here)")
+    z <- cacheSolve(m)
+    print(z)
+
+    print("-- get the inverse (shoud not be NULL)")
+    z <- m$getinverse()
+    print(z)
+
+    print("-- multiply M by its inverse (should obtain identity matrix)")
+    z <- m$get() %*% m$getinverse()
+    print(z)
+}
 
